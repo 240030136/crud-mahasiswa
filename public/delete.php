0a86403074f18a19c901839f0959cbebcf9bf223
240030136 <dendyariswastikas@gmail.com>
2025-12-03 22:34:44 +0800

    Update delete.php
    perbaikan typo

--- public/delete.php
+++ public/delete.php
@@ -7,7 +7,7 @@ $db = $database->getConnection();
 
 $mahasiswa = new Mahasiswa($db);
 
-// Pastikan ID ada
+
 if (!isset($_GET['id'])) {
     header("Location: index.php");
     exit;
@@ -15,14 +15,12 @@ if (!isset($_GET['id'])) {
 
 $mahasiswa->id = $_GET['id'];
 
-// Ambil data dulu untuk ambil nama file foto (opsional)
 $mahasiswa->readOne();
 $fotoLama = $mahasiswa->foto;
 
-// Hapus dari database
 if ($mahasiswa->delete()) {
 
-    // Hapus file foto (opsional)
+
     if ($fotoLama && file_exists("../uploads/" . $fotoLama)) {
         unlink("../uploads/" . $fotoLama);
     }
